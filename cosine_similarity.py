c3082f226941eddedb05fe1760d3150b77125b24
Siegmeyer <herkaiwo@gmail.com>
2017-06-24 16:44:26 +0200

    Linkage matrix to internal representation of dendrogram

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -44,10 +44,37 @@ def load_ratings(path):
     return (ratings, items)
 
 
+def relabel(dictionary):
+    count = 0
+    ret = {}
+    
+    for key in dictionary.keys():            
+        ret[count] = dictionary[key]
+        count += 1
+
+    return ret
+
+
+class cluster:
+    def __init__(self, id=-1):
+        self.id = id
+        self.parent = None
+        self.children = set([])
+
+    @property
+    def nodes(self, except_for=None):
+        nodes = set()
+        if self.children:
+            for child in self.children:
+                if except_for == child:
+                    continue
+
+                nodes = nodes.union(child.nodes)
+            return nodes
+        return self.id
+
 global silent
 silent = False
-
-cluster = KMeans
 
 log('Loading rating data...', 0)
 
@@ -55,57 +82,65 @@ pack = load_ratings('./data/filmtrust_ratings.dat')
 ratings = pack[0]
 items = pack[1]
 
-nrows = max(items) + 1
-ncols = max(ratings) + 1
+nrows = max(ratings)
+ncols = max(items) + 1
 
 log('Bulding sparse matrix...', 0, 1)
 
 R = dok_matrix((nrows, ncols), dtype=np.float32)
 
+ratings = relabel(ratings)
+
 for user in ratings:
     for item in ratings[user]:
-        R[item, user] = ratings[user][item]
+        R[user, item] = ratings[user][item]
 
 log('[rows - items]: {}'.format(nrows), 1)
 log('[columns - users]: {}'.format(ncols), 1)        
 log('[stored values]: {}'.format(R.nnz), 1, 1)
-log('Calculating cosine similarity...', 0)
 log('Clustering...', 0, 1)
 
+#R = np.array([[0.1,   2.5],
+#              [1.5,   .4 ],
+#              [0.3,   1  ],
+#              [1  ,   .8 ],
+#              [0.5,   0  ],
+#              [0  ,   0.5],
+#              [0.5,   0.5],
+#              [2.7,   2  ],
+#              [2.2,   3.1],
+#              [3  ,   2  ],
+#              [3.2,   1.3]])
 
-#X = R.toarray()
+Z = hierarchy.linkage(R.toarray(), method='ward')
 
+clusters = {}
 
-#X = np.concatenate([np.random.randn(3, 10), np.random.randn(2, 10) + 100])
-#model = FeatureAgglomeration()
-#model.fit(X)
+for ind, row in enumerate(Z):
+    for i in range(2):
+        if not row[i] in clusters:
+            clusters[row[i]] = cluster(row[i])
+           
+    A = clusters.get(row[0])
+    B = clusters.get(row[1])
+    
+    C = cluster(nrows + ind)
+    C.children.add(A)
+    C.children.add(B)
 
-#ii = itertools.count(X.shape[0])
-#x = [{'node_id': next(ii), 'left': x[0], 'right':x[1]} for x in model.children_]
+    clusters[C.id] = C
 
-#print(model.labels_)
-#for l in model.labels_:
-#    print(l)
-#print(x)
+print(len(clusters))
+        
 
-
-
-#cossim = cosine_similarity(R.transpose())
-#labels = cluster(2).fit_predict(cossim)
-
-R = R.transpose().toarray()
-
-print(R)
-
-Z = hierarchy.linkage(R, method='single', metric='cosine')
-plt.figure()
-dn = hierarchy.dendrogram(Z)
-hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
-fig, axes = plt.subplots(1, 2, figsize=(8, 3))
-dn1 = hierarchy.dendrogram(Z, ax=axes[0], above_threshold_color='y',
-                           orientation='top')
-dn2 = hierarchy.dendrogram(Z, ax=axes[1], above_threshold_color='#bcbddc',
-                           orientation='right')
-hierarchy.set_link_color_palette(None)  # reset to default after use
-plt.show()
+#plt.figure()
+#dn = hierarchy.dendrogram(Z)
+#hierarchy.set_link_color_palette(['m', 'c', 'y', 'k'])
+#fig, axes = plt.subplots(1, 2, figsize=(8, 3))
+#dn1 = hierarchy.dendrogram(Z, ax=axes[0], above_threshold_color='y',
+#                           orientation='top')
+#dn2 = hierarchy.dendrogram(Z, ax=axes[1], above_threshold_color='#bcbddc',
+#                           orientation='right')
+#hierarchy.set_link_color_palette(None)  # reset to default after use
+#plt.show()
 
